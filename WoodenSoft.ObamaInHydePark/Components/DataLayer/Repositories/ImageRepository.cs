985c6b37d3b14a56bf15c61eb779519a4d93227c
awood@visionps.com <awood@visionps.com>
2012-07-10 16:31:32 -0500

    Fixed more bugs with the theme

---
+++ WoodenSoft.ObamaInHydePark/Components/DataLayer/Repositories/ImageRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WoodenSoft.ObamaInHydePark.Components.DataLayer.Common;
+using WoodenSoft.ObamaInHydePark.Components.DataLayer.Models;
+using WoodenSoft.ObamaInHydePark.Components.DataLayer.Repositories.Interfaces;
+
+namespace WoodenSoft.ObamaInHydePark.Components.DataLayer.Repositories
+{
+    public class ImageRepository : GenericRepository<Image>, IImageRepository
+    {
+        public void Save(Image img)
+        {
+            var sqlParams = new[] {new SqlParameter("@name", img.Name), new SqlParameter("@url", img.Url)};
+            ExecuteNonQuery("INSERT INTO Images(Name, Url) Values(@name, @url)", sqlParams);
+        }
+
+        public void Delete(int id)
+        {
+            ExecuteNonQuery("DELETE FROM Images WHERE Id = " + id);
+        }
+
+        public IEnumerable<Image> GetAll()
+        {
+            return ExecuteQuery("SELECT * FROM Images").DataTableToList<Image>(new Image());
+        }
+    }
+}
---
+++ WoodenSoft.ObamaInHydePark/Components/DataLayer/Repositories/Interfaces/IImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WoodenSoft.ObamaInHydePark.Components.DataLayer.Models;
+
+namespace WoodenSoft.ObamaInHydePark.Components.DataLayer.Repositories.Interfaces
+{
+    public interface IImageRepository
+    {
+        void Save(Image img);
+        void Delete(int id);
+        IEnumerable<Image> GetAll();
+    }
+}
